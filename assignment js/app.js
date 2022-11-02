054a3602ed10e2ce710880f2d7c72489d2ad23b4
ahmergit <117104570+ahmergit@users.noreply.github.com>
2022-11-02 16:20:42 +0500

    Add files via upload

---
+++ assignment js/app.js
@@ -0,0 +1,62 @@
+// var a=10;
+// var b=20;
+// var c=a+b;
+
+// alert(c);
+
+// var a=40;
+// var b=30;
+// console.log(a+b);
+
+
+
+// var n="20"
+
+// var a="30"
+//  console.log(n + a);
+
+// var a=10;
+// var b=20;
+// var c=a+b;
+
+// alert(c);
+
+// var a=220;
+// var b=310;
+// console.log(a+b);
+
+// var n="20"
+
+// var a="340"
+//  console.log(n + a);
+
+// var n="20"
+
+// var a="32"
+//  console.log(n + a);
+
+
+
+
+// var a = 20;
+// a++;
+// console.log(a);
+
+// var a = 235;
+// var d=4
+// var c = d+a;
+// a++;
+// console.log(c);
+// console.log(a);
+
+
+// var b=21;
+// ++b;
+// var c=22;
+// var d = c-b
+// console.log(d);
+
+var a="hello"
+var b="world"
+alert(a+b);
+console.log(a+b);
